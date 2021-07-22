27b0be4c41c916b9b9bfc130f471eb2b995c5452
adam-osu <okashaa@oregonstate.edu>
2021-07-22 01:01:12 -0400

    Update db config, update components

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Example } from "./shared/Example";
 
-const App = () => <h1>Hello World!</h1>;
+const App = () => (
+  <>
+    <h1>Hello World!</h1>
+    <Example />
+  </>
+);
 
 export default App;
---
+++ app/javascript/components/shared/Example.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Example = () => (
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in quia
+    doloribus culpa porro! Laudantium, eaque, quibusdam consectetur suscipit
+    temporibus molestiae ab perspiciatis ex impedit excepturi deserunt illum
+    expedita ad?
+  </p>
+);
