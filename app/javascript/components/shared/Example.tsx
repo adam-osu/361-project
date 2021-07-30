c8d14e3757dc1e55a609944647b9d9f76a118f20
adam-osu <okashaa@oregonstate.edu>
2021-07-30 13:11:34 -0400

    Add styled-components

--- app/javascript/components/shared/Example.tsx
+++ app/javascript/components/shared/Example.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledParagraph = styled.p`
+  color: blue;
+`;
 
 export const Example = () => (
-  <p>
+  <StyledParagraph>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in quia
     doloribus culpa porro! Laudantium, eaque, quibusdam consectetur suscipit
     temporibus molestiae ab perspiciatis ex impedit excepturi deserunt illum
     expedita ad?
-  </p>
+  </StyledParagraph>
 );
