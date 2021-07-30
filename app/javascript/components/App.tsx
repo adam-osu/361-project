ba9dcca52df0eb445aa1f4f3f987714b4dae1cdf
adam-osu <okashaa@oregonstate.edu>
2021-07-30 19:39:43 -0400

    Add styled-reset

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Router } from "./Router";
 import { Routes } from "./Routes";
+import { Reset } from "styled-reset";
 
 const App = (props) => (
   <Router path={props.path}>
+    <Reset />
     <Routes />
   </Router>
 );
