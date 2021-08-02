60af2a250736d32b840c1945f96f120e0accf4fa
adam-osu <okashaa@oregonstate.edu>
2021-08-01 22:45:24 -0400

    Update Categories: get data

---
+++ app/javascript/shared/components/Table/Table.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+  border: 1px solid;
+`;
+
+export const TableHead = styled.thead`
+  border: 1px solid;
+  text-align: left;
+`;
+
+export const TableCell = styled.th`
+  text-align: left;
+`;
