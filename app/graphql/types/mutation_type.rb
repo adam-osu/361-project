90744c1e365c6107cb37d3b27d737e30a44b576a
adam-osu <okashaa@oregonstate.edu>
2021-08-10 23:47:54 -0400

    Add expense delete mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,6 +4,7 @@ module Types
     field :signin_user, mutation: Mutations::SignInUser
     field :create_expense, mutation: Mutations::CreateExpense
     field :update_expense, mutation: Mutations::UpdateExpense
+    field :delete_expense, mutation: Mutations::DeleteExpense
     field :create_category, mutation: Mutations::CreateCategory
   end
 end
