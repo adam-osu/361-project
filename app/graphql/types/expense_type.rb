6ee73daceaeebdf2b01b0ec963925ca6f011bcd3
adam-osu <okashaa@oregonstate.edu>
2021-08-10 23:32:20 -0400

    Add UpdateExpense mutation

--- app/graphql/types/expense_type.rb
+++ app/graphql/types/expense_type.rb
@@ -1,5 +1,6 @@
 module Types
   class ExpenseType < Types::BaseObject
+    field :id, Integer, null: false
     field :title, String, null: false
     field :amount, Float, null: false
     field :category, Types::CategoryType, null: true
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@ module Types
     field :create_user, mutation: Mutations::CreateUser
     field :signin_user, mutation: Mutations::SignInUser
     field :create_expense, mutation: Mutations::CreateExpense
+    field :update_expense, mutation: Mutations::UpdateExpense
     field :create_category, mutation: Mutations::CreateCategory
   end
 end
