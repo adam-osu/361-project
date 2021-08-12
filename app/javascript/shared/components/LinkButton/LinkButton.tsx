559a83a26305e013199a57c027c5074471fc884c
adam-osu <okashaa@oregonstate.edu>
2021-08-12 00:38:41 -0400

    Update LinkButton

--- app/javascript/shared/components/LinkButton/LinkButton.tsx
+++ app/javascript/shared/components/LinkButton/LinkButton.tsx
@@ -2,12 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const buttonStyles = {
+  small: {
+    padding: ".25em 1em",
+    fontSize: "12px",
+  },
+  medium: {
+    padding: "0.5em 2em",
+    fontSize: "16px",
+  },
+  primary: {
+    background: "blue",
+  },
+  secondary: {
+    background: "darkgrey",
+  },
+};
+
 const StyledLink = styled(Link)`
   display: flex;
   justify-content: center;
-  padding: 0.5em 2em;
-  background: blue;
-  font-size: 16px;
+  padding: ${(props) => buttonStyles[props.size].padding};
+  background: ${(props) => buttonStyles[props.variant].background};
+  font-size: ${(props) => buttonStyles[props.size].fontSize};
   font-weight: 500;
   color: #fff;
   text-decoration: none;
@@ -16,10 +33,29 @@ const StyledLink = styled(Link)`
 `;
 
 interface LinkButtonProps {
+  variant?: "primary" | "secondary" | "outline";
+  size?: "small" | "medium";
   linkTo: string;
   text: string;
+  state?: any;
 }
 
-export const LinkButton = ({ linkTo, text }: LinkButtonProps) => {
-  return <StyledLink to={linkTo}>{text}</StyledLink>;
+export const LinkButton = ({
+  variant = "primary",
+  size = "medium",
+  linkTo,
+  text,
+  state = null,
+}: LinkButtonProps) => {
+  return (
+    <StyledLink
+      size={size}
+      variant={variant}
+      // to={linkTo}
+      to={{ pathname: linkTo, state }}
+      // state={state}
+    >
+      {text}
+    </StyledLink>
+  );
 };
