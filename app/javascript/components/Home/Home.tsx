3d2c83c5e46b7c756ac59ffed6915d336f8ab3d8
adam-osu <okashaa@oregonstate.edu>
2021-08-13 19:58:38 -0400

    Updates Home

--- app/javascript/components/Home/Home.tsx
+++ app/javascript/components/Home/Home.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 import { useAuth } from "../Auth";
 
 export const Home = () => {
   const { user } = useAuth();
-  return user ? <h1>Welcome {user.firstName}!</h1> : <h1>Home</h1>;
+  return user ? <Redirect to="/expenses" /> : <Redirect to="/login" />;
 };
