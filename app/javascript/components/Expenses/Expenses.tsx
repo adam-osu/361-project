8961fe26d3b30c7da3e6f86149b354c1039f7672
adam-osu <okashaa@oregonstate.edu>
2021-08-12 22:45:36 -0400

    Add delete to Expenses

--- app/javascript/components/Expenses/Expenses.tsx
+++ app/javascript/components/Expenses/Expenses.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 
 import { useAuth } from "../Auth";
 import { LinkButton } from "../../shared/components/LinkButton";
 import { Table, TableHead, TableCell } from "../../shared/components/Table";
+import { Button } from "../../shared/components/Button";
 
 const GET_EXPENSES = gql`
   query getExpenses {
@@ -19,13 +20,30 @@ const GET_EXPENSES = gql`
   }
 `;
 
+const DELETE_EXPENSE = gql`
+  mutation deleteExpense($id: Int!) {
+    deleteExpense(id: $id) {
+      status
+    }
+  }
+`;
+
 export const Expenses = () => {
   const { loading, error, data, refetch } = useQuery(GET_EXPENSES);
+  const [
+    deleteExpense,
+    { loading: deleteLoading, error: deleteError, data: deleteData },
+  ] = useMutation(DELETE_EXPENSE);
+
   const { user } = useAuth();
 
   useEffect(() => {
     refetch();
-  }, []);
+  }, [deleteData]);
+
+  const onDeleteExpense = (id) => {
+    return () => deleteExpense({ variables: { id: id } });
+  };
 
   return (
     <>
@@ -34,7 +52,7 @@ export const Expenses = () => {
       ) : (
         <h1>Expenses</h1>
       )}
-      <LinkButton linkTo="/expenses/new" text="Add Expense" />
+      <LinkButton linkTo="/expenses/new">Add Expense</LinkButton>
       {data?.expenses ? (
         <Table>
           <TableHead>
@@ -43,6 +61,7 @@ export const Expenses = () => {
             <th>Category</th>
             <th>Date</th>
             <th>Update</th>
+            <th>Delete</th>
           </TableHead>
           <tbody>
             {data.expenses.map((expense) => (
@@ -55,14 +74,24 @@ export const Expenses = () => {
                   <TableCell>N / A</TableCell>
                 )}
                 <TableCell>TBD</TableCell>
-                <TableCell>
+                <TableCell contained>
                   <LinkButton
                     variant={"secondary"}
                     size="small"
                     linkTo="/expenses/edit"
-                    text="Update"
                     state={expense}
-                  />
+                  >
+                    Update
+                  </LinkButton>
+                </TableCell>
+                <TableCell contained>
+                  <Button
+                    onClick={onDeleteExpense(expense.id)}
+                    variant={"danger"}
+                    size="small"
+                  >
+                    Delete
+                  </Button>
                 </TableCell>
               </tr>
             ))}
